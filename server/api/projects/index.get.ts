a52a25a856bd64c24735206c80521d698e0b951d
John Million <john.million@rtslabs.com>
2026-04-18 20:49:20 -0400

    Drop personal projects and add resume data

--- server/api/projects/index.get.ts
+++
@@ -1,7 +0,0 @@
-import { db } from '../../database'
-import { projects } from '../../database/schema'
-
-export default defineEventHandler(async () => {
-  const allProjects = await db.select().from(projects)
-  return allProjects
-})
